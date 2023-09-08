d44e01c11956ba01b721594c5ba8f184fa7b964e
olthapage <olthar.rosyeda01@gmail.com>
2023-09-08 15:17:44 +0700

    simpan kode java ke Github

--- MyFirstJavaGithub26.java
+++ MyFirstJavaGithub26.java
@@ -5,7 +5,7 @@ public class MyFirstJavaGithub26 {
 
     public static void main(String[] args) {
         System.out.println("Hello from Github");
-        
+        System.out.println("ini branch daspro");
     }
 }
 
